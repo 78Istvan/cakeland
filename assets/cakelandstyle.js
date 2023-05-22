859874c51aadb9a8383ef6292cfb309720c5edb8
78Istvan <istvan78juhasz@gmail.com>
2023-05-22 23:07:17 +0100

    cake gallery pages are created

---
+++ assets/cakelandstyle.js
@@ -0,0 +1,28 @@
+$(".slider_container").slick({
+  dots: true,
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  autoplay: true,
+  speed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
