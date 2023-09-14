be6ef802c626245982d0847034fed2b5219e817f
78Istvan <istvan78juhasz@gmail.com>
2023-09-14 21:37:09 +0100

    availability html page cleared out

--- assets/availability.js
+++ assets/availability.js
@@ -1,15 +1,1 @@
-$(".slider_container").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  fade: true,
-  asNavFor: ".slider_nav",
-});
-$(".slider_nav").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: ".slider_container",
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true,
-});
+
