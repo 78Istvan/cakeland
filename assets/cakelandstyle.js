881eebb24c1cf8cff76d51379372daf1fb04d5d8
78Istvan <istvan78juhasz@gmail.com>
2023-09-23 21:11:29 +0100

    add gallery to cakelandstyle html page

--- assets/cakelandstyle.js
+++ assets/cakelandstyle.js
@@ -26,3 +26,15 @@ $(".slider_container").slick({
     },
   ],
 });
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic) {
+  fullImgBox.style.display = "flex";
+  fullImg.src = pic;
+}
+
+function closFullImg() {
+  fullImgBox.style.display = "none";
+}
