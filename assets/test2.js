0ef162762a65b997dadfc4b3b500f5c6b43947c4
78Istvan <istvan78juhasz@gmail.com>
2023-09-23 18:08:21 +0100

    stest3 page gallery created

--- assets/test2.js
+++ assets/test2.js
@@ -1,37 +1,27 @@
-var gallery = document.querySelector("#gallery");
-var getVal = function (elem, style) {
-  return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
-};
-var getHeight = function (item) {
-  return item.querySelector(".content").getBoundingClientRect().height;
-};
-var resizeAll = function () {
-  var altura = getVal(gallery, "grid-auto-rows");
-  var gap = getVal(gallery, "grid-row-gap");
-  gallery.querySelectorAll(".gallery-item").forEach(function (item) {
-    var el = item;
-    el.style.gridRowEnd =
-      "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
-  });
-};
-gallery.querySelectorAll("img").forEach(function (item) {
-  item.classList.add("byebye");
-  if (item.complete) {
-    console.log(item.src);
-  } else {
-    item.addEventListener("load", function () {
-      var altura = getVal(gallery, "grid-auto-rows");
-      var gap = getVal(gallery, "grid-row-gap");
-      var gitem = item.parentElement.parentElement;
-      gitem.style.gridRowEnd =
-        "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-      item.classList.remove("byebye");
-    });
+let input = document.getElementById("input");
+
+const handleSearch = (e) => {
+  let inputValue = e.target.value.toLowerCase();
+  let array = document.getElementsByClassName("item");
+
+  for (let i = 0; i < array.length; i++) {
+    let image = array[i];
+    let caption = image.getAttribute("data-caption").toLowerCase();
+    let title = image.getAttribute("title").toLowerCase();
+    caption.includes(inputValue) || title.includes(inputValue)
+      ? (image.style.display = "inline")
+      : (image.style.display = "none");
   }
-});
-window.addEventListener("resize", resizeAll);
-gallery.querySelectorAll(".gallery-item").forEach(function (item) {
-  item.addEventListener("click", function () {
-    item.classList.toggle("full");
+};
+input.addEventListener("keyup", handleSearch);
+
+window.addEventListener("load", function () {
+  baguetteBox.run(".gallery", {
+    captions: true,
+    buttons: "auto",
+    animation: "fadeIn",
   });
+});
+window.addEventListener("load", function () {
+  baguetteBox.run(".gallery");
 });
